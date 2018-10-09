199137b76aef736259fdc81075ce0e93dc2353c6
Sam <sam.phone.now@gmail.com>
2018-10-09 11:51:38 -0500

    Updating files with useful comments
    Signed-off-by: Sam <sam.phone.now@gmail.com>

--- Leetcode/src/main/java/BackTracking/Nqueens.java
+++ Leetcode/src/main/java/BackTracking/Nqueens.java
@@ -1,3 +1,7 @@
+// Isaac Samuel Raj Boddu
+// leetcode #51 (n-Queens all solutions)
+
+
 package BackTracking;
 
 import java.util.ArrayList;
@@ -5,32 +9,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Nqueens {
+
+    // Main method
     public static void main(String[] args) {
         // Set the number of queens/board dimensions here
         int queens = 5;
 
-        List<List<String>> sols = new ArrayList<List<String>>();
-        char[][] board = generateBoard(queens);
+        // Create a solutions List
+        List<List<String>> sols = new ArrayList<>();
 
-        // printBoard(board);
+        // Board holding current state
+        char[][] board = generateBoard(queens);
 
         solve(board, sols, 0, queens);
 
-        sols.forEach(solution -> System.out.println(solution));
+        sols.forEach(System.out::println);
 
     }
 
     public static void printBoard(char[][] b){
+        // Utility function to print the current state of the board
 
+        // Print each row from the board to console
         for(char[] st : b){
             System.out.println(st);
         }
 
     }
 
-    public static char[][] generateBoard(int size){
+    private static char[][] generateBoard(int size){
+        // Generates a new size*size board
+
         char[][] board = new char[size][size];
 
+        // Fill the empty board with '.' representing vacant position
         for(int i = 0; i < size; ++i){
             Arrays.fill(board[i], '.');
         }
@@ -38,28 +50,35 @@ public class Nqueens {
         return board;
     }
 
-    public static boolean solve(char[][] b, List<List<String>> sols, int col, int size){
-        if (col == b.length){
-            // Save the state of the board
-            //printBoard(b);
+    private static boolean solve(char[][] b, List<List<String>> sols, int col, int size){
+        // Solve for all possible solutions for n-Queens problem
 
-            // Add to the sol list
-            List<String> solution = new ArrayList<String>();
+        if (col == b.length){
+            // All queens have been placed so,
+            // Save the state of the board
+
+            // Format the board as required (In this case as a list of strings)
+            List<String> solution = new ArrayList<>();
             for(char[] row : b){
                 String rowString = String.copyValueOf(row);
                 solution.add(rowString);
             }
 
+            // Add to the sol list
             sols.add(solution);
-            //return true;
+
         }
 
+        // Starting with the first row, try placing a queen at each row and given col number
         for(int row = 0; row < b.length; ++row){
+
+            // Only if the move is valid, place a queen
             if (isSafe(b, row, col, size)){
+
                 // Place the queen at this position and solve the board from the next column
                 b[row][col] = 'Q';
 
-                // If failed to solve undo the placement and continue in the current loop
+                // If failed to solve undo the placement and continue in the current loop (Backtrack!)
                 if (!solve(b, sols, col+1, size))
                     b[row][col] = '.';
 
@@ -70,24 +89,26 @@ public class Nqueens {
 
     }
 
-    public static Boolean isSafe(char[][] b, int row, int col, int size){
+    private static Boolean isSafe(char[][] b, int row, int col, int size){
+        // Utility function which returns True if placing a queens at location b[row][col] is valid, False otherwise
 
-        // Check to the left in the row
+        // Check towards the left in the row
         for(int i = 0; i < col; ++i)
             if (b[row][i] == 'Q') return false;
 
-        // Check to the top in the col
+        // Check towards the top in the col
         for(int i = 0; i < row; ++i)
             if (b[i][col] == 'Q') return false;
 
-        // Check to the diagonal top back left
+        // Check towards the diagonal top back left
         for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
             if (b[i][j] == 'Q') return false;
 
-        // Check to the diagonal bottom back left
+        // Check towards the diagonal bottom back left
         for(int i = row + 1, j = col - 1; i < size && j >= 0; ++i, --j)
             if (b[i][j] == 'Q') return false;
 
+        // All checks have passed
         return true;
     }
 }
